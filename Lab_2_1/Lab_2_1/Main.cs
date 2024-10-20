dbb17c7026c92b1eb68c67dc86feda2c8f73c108
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-10-21 01:19:07 +0300

    merged into one project

---
+++ Lab_2_1/Lab_2_1/Main.cs
@@ -0,0 +1,77 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //тест конструктора з двовимірного масиву
+        double[,] array = {
+            { 1.0, 2.0, 3.0 },
+            { 4.0, 5.0, 6.0 },
+            { 7.0, 8.0, 9.0 }
+        };
+        MyMatrix matrix1 = new MyMatrix(array);
+        Console.WriteLine("тест конструктора з двовимірного масиву");
+        Console.WriteLine(matrix1);
+
+        //тест конструктора копіювання
+        MyMatrix matrix2 = new MyMatrix(matrix1);
+        Console.WriteLine("тест конструктора копіювання");
+        Console.WriteLine(matrix2);
+
+        //тест конструктора із зубчастого масиву
+        double[][] jaggedArray = {
+            new double[] { 1.1, 2.2, 3.3 },
+            new double[] { 4.4, 5.5, 6.6 },
+            new double[] { 7.7, 8.8, 9.9 }
+        };
+        MyMatrix matrix3 = new MyMatrix(jaggedArray);
+        Console.WriteLine("тест конструктора із зубчастого масиву");
+        Console.WriteLine(matrix3);
+
+        //тест конструктора з масиву рядків
+        string[] lines = {
+            "10 20 30",
+            "40 50 60",
+            "70 80 90"
+        };
+        MyMatrix matrix4 = new MyMatrix(lines);
+        Console.WriteLine("тест конструктора з масиву рядків");
+        Console.WriteLine(matrix4);
+
+        //тест конструктора з рядка
+        string input = "11 12 13\n14 15 16\n17 18 19";
+        MyMatrix matrix5 = new MyMatrix(input);
+        Console.WriteLine("тест конструктора з рядка");
+        Console.WriteLine(matrix5);
+
+        //тест індексаторів
+        Console.WriteLine("тест індексаторів: [1,1] = " + matrix1[1, 1]);
+        matrix1[1, 1] = 55.0;
+        Console.WriteLine("тест індексаторів: [1,1] (сетнули значення 55.0) = " + matrix1[1, 1]);
+
+        //тест додавання матриць
+        MyMatrix matrixSum = matrix1 + matrix2;
+        Console.WriteLine("сума матриць:");
+        Console.WriteLine(matrixSum);
+
+        //тест множення матриць
+        double[,] array2 = {
+            { 1, 2 },
+            { 3, 4 },
+            { 5, 6 }
+        };
+        MyMatrix matrix6 = new MyMatrix(array2);
+        MyMatrix matrixProduct = matrix1 * matrix6;
+        Console.WriteLine("добуток матриць:");
+        Console.WriteLine(matrixProduct);
+
+        //тест транспонування
+        MyMatrix transposedMatrix = matrix1.GetTransponedCopy();
+        Console.WriteLine("транспонована матриця:");
+        Console.WriteLine(transposedMatrix);
+
+        //тест TransponeMe
+        matrix1.TransponeMe();
+        Console.WriteLine("транспонування TransponeMe:");
+        Console.WriteLine(matrix1);
+    }
+}
---
+++ Lab_2_1/Lab_2_1/MatrixOperations.cs
@@ -0,0 +1,57 @@
+﻿public partial class MyMatrix
+{
+    //operator + додавання двох матриць (лише якщо вони мають однаковий розмір)
+    public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+    {
+        var result = new MyMatrix(new double[a.Height, a.Width]); //матриця що утвор. від додавання
+
+        for (int i = 0; i < a.Height; i++) 
+            for (int j = 0; j < a.Width; j++)
+                result[i, j] = a[i, j] + b[i, j];
+
+        return result;
+    }
+    //operator * множення двох матриць (лише якщо кількість стовпчиків першої
+    //дорівнює кількості рядків другої)
+    public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+    {
+        var result = new MyMatrix(new double[a.Height, b.Width]);
+        //проходить по всіх рядках першої матриці
+        for (int i = 0; i < a.Height; i++) //по рядках матриці а
+            //проходить по всіх стовпцях другої матриці
+            for (int j = 0; j < b.Width; j++)  //по елем. матриці б.
+                //проходить по всіх елементах рядка першої і стовпця другої
+                for (int k = 0; k < a.Width; k++) //по елем. матриці а.
+                    result[i, j] += a[i, k] * b[k, j];
+
+        return result;
+    }
+    //Метод(не статичний; private або protected) GetTransponedArray(), що
+    //повертає новостворений масив double[,] (не MyMatrix, а просто масив), у якому
+    //вміст елементів транспонований відносно тієї матриці, для якої він викликався
+    private double[,] GetTransponedArray()
+    {
+        var transposed = new double[Width, Height];
+
+        for (int i = 0; i < Height; i++)
+            for (int j = 0; j < Width; j++)
+                transposed[j, i] = data[i, j];
+
+        return transposed;
+    }
+    //Метод (не статичний) MyMatrix GetTransponedCopy(), який би створював
+    //новий примірник MyMatrix, у якому вміст матриці транспонований відносно тієї,
+    //для якої він викликався; технічну роботу зі власне транспонування не повторювати,
+    //а використати результат GetTransponedArray()
+    public MyMatrix GetTransponedCopy()
+    {
+        return new MyMatrix(GetTransponedArray());
+    }
+    //Метод (не статичний) void TransponeMe(), який би замінював матрицю, для якої
+    //викликається, на транспоновану(теж використати GetTransponedArray(), але
+    //щоб у результаті змінився сам this-примірник MyMatrix).
+    public void TransponeMe()
+    {
+        data = GetTransponedArray();
+    }
+}
